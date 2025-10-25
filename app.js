d6c0913965977c5372ba1e672d37bf1c448ae591
Sebastian Collado <sebastiancolladott@gmail.com>
2025-10-25 11:44:46 -0300

    .

--- app.js
+++ app.js
@@ -129,12 +129,15 @@ app.post("/api/auth/change-password", requireAuth, async (req, res) => {
 
 // ---------- Recuperación de contraseña ----------
 const transporter = nodemailer.createTransport({
-  service: "smtp.office365.com",
+  host: "smtp.office365.com",
   port: 587,
   secure: false,
   auth: {
     user: "sebastiancolladott@outlook.com",
     pass: "xhvycclyxgfrfyhl",
+  },
+  tls: {
+    ciphers: "SSLv3",
   },
 });
 
