addc2d7518487dfea05736a3066830f31b12c127
Sebastian Collado <sebastiancolladott@gmail.com>
2025-11-02 18:34:50 -0300

    .

--- app.js
+++ app.js
@@ -40,24 +40,25 @@ if (!fs.existsSync(uploadDir)) {
 }
 
 // ---------- Middlewares ----------
-const allowedOrigins = [
-  "https://tijeras.imeatara.com",
-  "http://localhost:5173",
-];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // permitir requests sin "Origin" (como en curl o Postman)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) return callback(null, true);
-      callback(new Error("No permitido por CORS"));
-    },
+    origin: "https://tijeras.imeatara.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.options("*", cors()); // preflight Safari
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://tijeras.imeatara.com");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 app.disable("x-powered-by");
